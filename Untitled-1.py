554ea5a3dcb3052cfa55389aece4297ab8543d8d
vatikalashashank <vatikalashashank@gmail.com>
2024-12-26 09:05:58 +0530

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,34 @@
+# q=['1.what is the capital if india?',
+#    '2.what is tnhe national animal']
+# ans=['delhi','tiger']
+# for x in q:
+#     print(x)
+#     user_ans=input('enter answer:')
+#     if user_ans in ans:
+#         print('correct ans')
+#     else:
+#         print('worng ans')
+
+# corr_ans=0
+# wrong_ans=0
+# q=['1.what is the capital if india?',
+#    '2.what is tnhe national animal']
+# ans=['delhi','tiger']
+# for x in q:
+#     print(x)
+#     user_ans=input('enter answer:')
+#     if user_ans in ans:
+#         corr_ans+=1
+#         print('correct ans')
+#     else:
+#         wrong_ans+=1
+#         print('worng ans')
+#     print('percentage',((corr_ans/(corr_ans+wrong_ans))*100))
+
+# att=[1,44,66,88,[66,77],90]
+# print(att[-1])
+
+
+
+
+
